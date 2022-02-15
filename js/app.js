8c8e198da6ae500c977f11cd84954a2cb53c1326
Johan GENOIS <jadoprog@hotmail.com>
2022-02-15 12:23:51 +0100

    Site de métafenfeu non fini

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+// window.onload = function() {
+//     document.getElementById('body').style.display = 'none';
+//     document.querySelector('body').classList.remove('body-app');
+// }
+$(window).ready(function (){
+    $('#body').css('display', 'none');
+    $('body').removeClass('body-app');
+});
